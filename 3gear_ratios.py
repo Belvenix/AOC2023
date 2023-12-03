4c812ce52b11d2c3db077b75ea354368f1f592ec
jbelter <jbelter@meritus.pl>
2023-12-03 21:53:58 +0100

    Solve second part of third puzzle using iteration over found gears and checking where there are exactly two overlapping parts

--- 3gear_ratios.py
+++ 3gear_ratios.py
@@ -12,6 +12,10 @@ EXAMPLE_SCHEMATIC = \
 """
 
 
+GEAR = "*"
+SYMBOLS = set(["%", "&", "#", "-", "+", "=", "@", GEAR, "$", "/"])
+
+
 def find_all_unique_symbols(input_: str):
     symbols = set()
     for c in input_:
@@ -21,7 +25,6 @@ def find_all_unique_symbols(input_: str):
     print(f"found symbols are: {symbols}")
     return symbols
 
-SYMBOLS = find_all_unique_symbols(EXAMPLE_SCHEMATIC)
 
 def is_outside(grid: list[list[str]], i: int, j: int):
     heigth, width = len(grid), len(grid[0])
@@ -54,7 +57,8 @@ def is_near_symbol(grid: list[list[str]], digit_i: int, digit_j: int, digits: in
     print(f"the grid part looks like this: \n{grid_part}")
     return found
 
-def main(schematic: str):
+
+def first_part(schematic: str):
     current_digit = None
     gear_ratio = 0
     grid = [c for c in [line for line in schematic.splitlines()]]
@@ -78,8 +82,79 @@ def main(schematic: str):
     print(f"Calculated gear ratio is: {gear_ratio}")
     return gear_ratio
 
+
+def find_all_gears(grid: list[list[str]]) -> list[tuple[int, int]]:
+    gear_positions = []
+    heigth, width = len(grid), len(grid[0])
+    for i in range(heigth):
+        for j in range(width):
+            if grid[i][j] == GEAR:
+                gear_positions.append((i, j))
+    return gear_positions
+
+
+def find_all_digits(grid: list[list[str]]):
+    found_digits = []
+    current_digit = None
+    heigth, width = len(grid), len(grid[0])
+    for i in range(heigth):
+        for j in range(width):
+            c = grid[i][j]
+            if not str.isdigit(c):
+                if current_digit is not None:
+                    n_digits = len(current_digit)
+                    current_digit = int(current_digit)
+                    if j == 0:
+                        digit_i_position, digit_j_position = i - 1, width - n_digits
+                    else:
+                        digit_i_position, digit_j_position = i, j - n_digits
+                    # print(f"Found number: {current_digit} at ({digit_i_position},{digit_j_position})")
+                    if is_near_symbol(grid, digit_i_position, digit_j_position, n_digits):
+                        found_digits.append((current_digit, (digit_i_position, digit_j_position, n_digits)))
+                    current_digit = None
+            else:
+                current_digit = ''.join([current_digit or '', c])
+    return found_digits
+
+
+def is_part_overlapping_gear(part: tuple[int, tuple[int, int, int]], gear: tuple[int, int]):
+    gear_i, gear_j = range(gear[0] - 1, gear[0] + 2), range(gear[1] - 1, gear[1] + 2)
+    _, (part_i, part_j, part_len) = part
+    for i in gear_i:
+        for j in gear_j:
+            part_positions = [(part_i, p_j) for p_j in range(part_j, part_j + part_len)]
+            if (i, j) in part_positions:
+                return True
+    else:
+        return False
+
+
+def find_overlapping_parts(parts: list[tuple[int, tuple[int, int, int]]], gear: tuple[int, int]):
+    overlapping_parts = []
+    for part in parts:
+        if is_part_overlapping_gear(part, gear):
+            overlapping_parts.append(part)
+    return overlapping_parts
+
+
+def second_part(schematic: str) -> int:
+    grid = [c for c in [line for line in schematic.splitlines()]]
+    gears = find_all_gears(grid)
+    parts = find_all_digits(grid)
+    # print(f"The gears are: {gears}")
+    # print(f"The found parts are: {parts}")
+    gear_ratio = 0
+    for gear in gears:
+        overlapping_parts = find_overlapping_parts(parts, gear)
+        if len(overlapping_parts) == 2:
+            # print(f"Adding combined parts: {overlapping_parts}")
+            gear_ratio += overlapping_parts[0][0] * overlapping_parts[1][0]
+    print(f"Found gear ratio is: {gear_ratio}")
+    return gear_ratio
+
+
 if __name__ == "__main__":
-    gear_ratio = main(EXAMPLE_SCHEMATIC)
+    gear_ratio = first_part(EXAMPLE_SCHEMATIC)
     assert gear_ratio == 4361
-    # find_all_unique_symbols(REAL_SCHEMATIC)
-    # gear_ratio = main(REAL_SCHEMATIC)
+    gear_ratio = second_part(EXAMPLE_SCHEMATIC)
+    assert gear_ratio == 467835
