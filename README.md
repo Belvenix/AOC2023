42638823af27ec045b58e77e6b6a3b612a3d0eb9
jbelter <jbelter@meritus.pl>
2023-12-08 18:53:48 +0100

    Add puzzle visualisations, readme and requirements

---
+++ README.md
@@ -0,0 +1,79 @@
+# Advent of Code 2023
+
+## Project Overview
+
+Welcome to my Advent of Code 2023 repository! Here, I've taken on the challenge of solving the Advent of Code puzzles using Python, focusing primarily on enhancing my programming skills. My approach leans towards using Python's built-in methods and libraries, but I'm open to leveraging additional libraries for optimization or complex calculations.
+
+
+## Goals and Philosophy
+
+* Efficiency: Aim to solve puzzles in a reasonable timeframe (ideally a few hours).
+* Simplicity: Start with brute force or simple solutions, and optimize only when necessary. This approach mirrors real-world problem-solving in a business context.
+
+
+## Repository Structure
+
+AOC2023/
+│
+├── 01_solution.py
+├── 02_solution.py
+├── 03_solution.py
+├── ...
+├── 25_solution.py
+│
+├── puzzle_visualisations/
+│   ├── day_01_visualisation.jpg
+│   ├── day_02_visualisation.jpg
+│   ├── ...
+│   └── day_25_visualisation.jpg
+│
+├── data/
+│   ├── example/
+│   │   ├── 01/
+│   │   ├── 02/
+│   │   ├── ...
+│   │   └── 25/
+│   └── puzzle_input/  # To be added
+│
+└── display_calendar.py  # Advent calendar visualization using PyQt5
+
+
+## Coding Standards
+
+### Linting and Formatting
+In this project, we are committed to maintaining a high standard of code quality. To achieve this, we use ruff and black for linting and formatting our code.
+
+* **ruff**: A fast and highly configurable Python linter that helps us identify and fix various coding issues, ensuring code adheres to the best practices and coding conventions.
+
+* **black**: Known as "The Uncompromising Code Formatter". Black takes care of code formatting so that we can focus on solving puzzles instead of worrying about stylistic nuances.
+
+### Usage
+
+In order to run these linters over the code, after the libraries are installed please run the following command.
+
+```sh
+black .   # Formats code in the root directory
+ruff check . --fix  # Lints and auto-fixes issues
+```
+
+
+## Dependencies
+* tqdm: For progress bars in scripts.
+* numpy: For numerical operations.
+* PyQt5: For GUI elements and visualizations.
+* ruff: For linting.
+* black: For code formatting.
+
+
+## Visualisation
+
+display_calendar.py: A mockup Advent calendar visualisation using PyQt5. In order to run it simply use this command.
+
+```sh
+python display_calendar.py
+```
+
+
+## NOTICE
+
+Image Credits: Visual representations of puzzles created using DALL·E, provided by OpenAI.
---
+++ display_calendar.py
@@ -0,0 +1,103 @@
+import os
+import re
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLabel,
+    QMainWindow,
+    QScrollArea,
+    QWidget,
+)
+
+MAX_WIDTH = 1600
+
+
+class CalendarWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle("Advent of Code - Puzzle Calendar")
+        self.setGeometry(100, 100, MAX_WIDTH, 800)  # Adjust size as needed
+
+        scroll = QScrollArea()
+        self.setCentralWidget(scroll)
+
+        widget = QWidget()
+        scroll.setWidget(widget)
+        scroll.setWidgetResizable(True)
+
+        # Load images
+        image_folder = "puzzle_visualisations"
+        image_files = [
+            os.path.join(image_folder, f)
+            for f in os.listdir(image_folder)
+            if f.endswith(".png")
+        ]
+
+        # Sort the files by the numeric prefix
+        image_files.sort(
+            key=lambda x: int(re.match(r"(\d+)", x.split("/")[-1]).group()),
+        )
+
+        # Create grid layout
+        grid_layout = QGridLayout()
+        widget.setLayout(grid_layout)
+
+        # Maximum width for images
+        max_width = int((MAX_WIDTH * 0.95) / 5)
+
+        # Puzzle names
+        puzzle_names = [
+            "Day 1: Trebuchet?!",
+            "Day 2: Cube Conundrum",
+            "Day 3: Gear Ratios",
+            "Day 4: Scratchcards",
+            "Day 5: If You Give A Seed A Fertilizer",
+            "Day 6: Wait For It",
+            "Day 7: Camel Cards",
+            "Day 8: Haunted Wasteland",
+        ]
+
+        # Extend the puzzle names list to 25 elements
+        puzzle_names.extend(["???" for _ in range(25 - len(puzzle_names))])
+
+        # Add labels and images to the grid
+        for i in range(25):
+            label_name = QLabel(puzzle_names[i])
+            label_name.setAlignment(Qt.AlignCenter)
+            grid_layout.addWidget(label_name, i // 5 * 2, i % 5)
+
+            if i < len(image_files):
+                pixmap = QPixmap(image_files[i])
+                scaled_pixmap = pixmap.scaled(
+                    max_width,
+                    pixmap.height(),
+                    Qt.KeepAspectRatio,
+                )
+                label_image = QLabel()
+                label_image.setPixmap(scaled_pixmap)
+            else:
+                label_image = QLabel("")
+
+            label_image.setAlignment(Qt.AlignCenter)
+            grid_layout.addWidget(label_image, i // 5 * 2 + 1, i % 5)
+
+        # Adjust widget size to grid layout
+        widget.adjustSize()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = CalendarWindow()
+    ex.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
